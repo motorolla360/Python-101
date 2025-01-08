4e0d8d951a26ae8b0a0f8b2d2ec5e290d0cd1fd9
motorolla360 <ersayn17@hotmail.com>
2025-01-08 10:57:48 +0500

    Add files via upload

--- hello.py
+++ hello.py
@@ -1,20 +1,28 @@
 print("Hello, World!")
 
+import time
+
 a_string = "HELLO YERS which integer here is a positive number?"
 print(a_string)
 
 # Request integers from the user
 variable1 = int(input("Please enter the first integer: "))
 variable2 = int(input("Please enter the second integer & note that next request will be with No prompt string included: "))
+# Delay before requesting variable3
+time.sleep(2)
+print("in Mickey's voice: Well, I am waiting for yor f*cking response...")
 variable3 = int(input()) # No prompt string included
 
 a_list = [1, 2, 3, variable1, variable2, variable3]
 print(a_list)
 
-for listvar in a_list:
-    if listvar > 0:
-        print(f"{listvar} is positive")
-    elif listvar < 0:
-        print(f"{listvar} is negative")
+for item in a_list:
+    if item == 45:
+        print(f"{item} is for MARIIOO")
+    elif item > 0:
+        print(f"{item} is positive")
+    elif item < 0:
+        print(f"{item} is negative")
     else:
-        print(f"{listvar} is zero")
+        print(f"{item} is zero")
+print("Вот как-то так...)")
